394ef42a7d8ab756cda094cf769bfa87bd17fcd9
Munarbek74 <munarbekshorokhov@gmail.com>
2022-08-18 22:23:55 +0600

    3 commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
         Animal[] animals = {
                 new Shark("Акула", 25),
@@ -20,26 +20,26 @@ public class Main {
 
         };
 
-        for (Animal a: animals) {
+        for (Animal a : animals) {
             if (a instanceof Animal)
-            a.method();
-            }
+                a.method();
+        }
 
         Shark[] sharks = new Shark[3];
-        sharks[0] = new Shark("Акула", 25);
-        sharks[1] = new Shark("Акула1", 25);
-        sharks[2] = new Shark("Акула2", 25);
         Turtle[] turtles = new Turtle[3];
-        turtles[0] = new Turtle("Тартилла", 55);
-        turtles[1] = new Turtle("Тартилла1", 55);
-        turtles[2] = new Turtle("Тартилла2", 55);
         Eagle[] eagles = new Eagle[3];
-        eagles[0] = new Eagle("Орел", 32);
-        eagles[1] = new Eagle("Орел1", 32);
-        eagles[2] = new Eagle("Орел2", 32);
 
+        for (int i = 0, j = 0; i < animals.length; i++) {
+
+            if (animals[i] instanceof Shark) {
+                sharks[j] = (Shark) animals[i];
+            } else if (animals[i] instanceof Turtle) {
+                turtles[j] = (Turtle) animals[i];
+            } else if (animals[i] instanceof Eagle) {
+                eagles[j] = (Eagle) animals[i];
+            }
         }
 
 
-    }
+    }}
 
